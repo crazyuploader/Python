07480b96a8cbd9fa78ef374fedd2fe2e2653dc67
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-11 21:43:10 +0530

    Add: Addition Program

---
+++ add_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print('///Program to Add two Number(s)///')
+print('Enter First Number: ')
+a=input()
+print('Enter Second Number: ')
+b=input()
+sum=int(a)+int(b)
+print('Addition of Entered Number(s) is = ',sum)
+print('Created by Jugal Kishore -- 2020')
