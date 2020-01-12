4335cca50dec8e9b811a3a58d15e0017db8e20b7
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-01-12 16:14:48 +0530

    Update: build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-echo Checking all the files with Pyflakes
-echo
+echo ""
+echo "Checking all the files with Pyflakes"
+echo ""
 for f in $(ls *.py); do echo "Checking $f"; echo; pyflakes $f; done
