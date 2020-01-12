a5e07bc8d67e8305491e1e9c31d928c4557d7b11
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-12 12:45:59 +0530

    Add: build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+clear
+echo Available Files -
+for f in $(ls *.py); do echo $f; done
+echo
+echo Enter File Name without -- Extension
+read fname
+if [[ -f ${fname}.py ]]; then
+    clear
+    echo Running $fname.py with Python3
+    sleep 2
+    clear
+    python3 $fname.py
+else
+    echo File Does not Exist
+    echo Exiting!!
+    sleep 1
+    clear
+    exit
+fi
+echo K Thanks, Bye!
