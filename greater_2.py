2130cc08c5bbead432b82a77ab9c98b3c0455884
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-14 20:52:43 +0530

    Fix: indents

--- greater_2.py
+++ greater_2.py
@@ -5,11 +5,10 @@ print('\nEnter First Number: ')
 a = input()
 print('\nEnter Second Number: ')
 b = input()
-if a>b:
+if a > b:
     print('\nGreater Number between {0} and {1} is = {2}.'.format(a, a, a))
-elif b>a:
+elif b > a:
     print('\nGreater Number between {0} and {1} is = {2}.'.format(a, a, b))
-else: 
+else:
     print('\n{0} and {1} are Equal!'.format(a, b))
 print('\nCreated by Jugal Kishore -- 2020')
-
