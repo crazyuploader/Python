42c7f77948a0757c0a9863c45b77ec7980b4b565
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-14 21:09:18 +0530

    Fix: variable

--- greater_2.py
+++ greater_2.py
@@ -6,9 +6,9 @@ a = input()
 print('\nEnter Second Number: ')
 b = input()
 if a > b:
-    print('\nGreater Number between {0} and {1} is = {2}.'.format(a, a, a))
+    print('\nGreater Number between {0} and {1} is = {2}.'.format(a, b, a))
 elif b > a:
-    print('\nGreater Number between {0} and {1} is = {2}.'.format(a, a, b))
+    print('\nGreater Number between {0} and {1} is = {2}.'.format(a, b, b))
 else:
     print('\n{0} and {1} are Equal!'.format(a, b))
 print('\nCreated by Jugal Kishore -- 2020')
