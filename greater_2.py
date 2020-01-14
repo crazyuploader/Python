ba08aa5717a5bee0d6970d1214a94031cb3f3690
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-14 20:44:43 +0530

    Add: greater_2.py

---
+++ greater_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+print('///Program to Display Greater Number///')
+print('\nEnter First Number: ')
+a = input()
+print('\nEnter Second Number: ')
+b = input()
+if a>b:
+    print('\nGreater Number between {0} and {1} is = {2}.'.format(a, a, a))
+elif b>a:
+    print('\nGreater Number between {0} and {1} is = {2}.'.format(a, a, b))
+else: 
+    print('\n{0} and {1} are Equal!'.format(a, b))
+print('\nCreated by Jugal Kishore -- 2020')
+
