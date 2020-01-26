8b8468d7744118c36078b224baa8d0ebea96b11b
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-26 17:17:17 +0530

    Fix: table.py

--- table.py
+++ table.py
@@ -2,6 +2,6 @@
 
 print('///Program to Display Table of Entered Number///')
 a = int(input('\nEnter a Number: '))
-for x in range(10):
-    print(a, " * ", x, " = ", a*x)
+for x in range(11):
+    print(a, "*", x, "= ", a * x)
 print('\nCreated by Jugal Kishore -- 2020')
