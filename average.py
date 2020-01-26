48e432a240882275b5f2eefd50bd6e5ba51b9ed2
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-26 17:00:34 +0530

    Update: average.py

--- average.py
+++ average.py
@@ -4,10 +4,10 @@ print('///Program to Display Average of Entered n Number(s)///')
 number, total = [], 0
 print('\nEnter Number of Numbers you want to get Average of: ')
 num = int(input())
+print("\nEnter ", num, " Number(s): ")
 for x in range(num):
     number.append(int(input()))
 total = sum(number)
-print(total)
 average = total / num
-print('\nAverage of Entered Number(s) is = ', average)
+print('\nAverage of Entered Number(s) is = ', round(average, 3))  # Limiting Decimal Precision to 3 Digits
 print('\nCreated by Jugal Kishore -- 2020')
