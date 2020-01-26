3ca02831bfe504b74798c119ab31403f828dfc31
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-26 16:59:53 +0530

    Update: build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 clear
 echo "Available Files -"
 for f in *.py; do echo "$f"; done
-echo
+echo ""
 echo "Enter File Name without -- Extension"
 read -r fname
 if [[ -f ${fname}.py ]]; then
@@ -19,4 +19,27 @@ else
     clear
     exit
 fi
-echo "K Thanks, Bye!"
+echo ""
+echo "Show Source Code?"
+echo ""
+echo "'y' for yes and anything else for no"
+read -r temp
+if [[ $temp = "y" ]]; then
+    clear
+    cat "${fname}.py"
+    sleep 5
+    echo ""
+    echo "Press 'y' to Exit"
+    read -r temp
+    if [[ $temp = "y" ]]; then
+        clear
+        echo "K Thanks, Bye!"
+        sleep 3
+        exit
+    fi
+else
+    clear
+    echo "K Thanks, Bye!"
+    sleep 2
+    exit
+fi
