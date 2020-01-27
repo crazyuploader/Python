266d629ea729799b46dfa0327f7f3bf5883d3b2b
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-27 12:07:23 +0530

    Update
    add_2.py
    average.py
    base_exponent_power.py
    division_2.py
    even_odd.py
    factorial.py
    greater_2.py
    greater_3.py
    hello.py
    multiplication_2.py
    palindrome.py
    prime.py
    reverse.py
    subtract_2.py
    table.py

--- division_2.py
+++ division_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__author__  = 'Jugal Kishore'
+__author__ = 'Jugal Kishore'
 __version__ = '1.0'
 
 print("///Program to Divide two Number(s)///")
--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__author__  = 'Jugal Kishore'
+__author__ = 'Jugal Kishore'
 __version__ = '1.0'
 
 print("///Program to check if Entered Number is Even or Odd///")
