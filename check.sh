cb6309f7f5628b94da97d1fd1a6dbdb5fd3c29aa
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-02-08 21:19:59 +0530

    Update:
    .circleci/config.yml
    .travis.yml
    check.sh
    number_conversion.py

--- check.sh
+++ check.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+# Colors
+GREEN='\033[1;32m'
+NC='\033[0m'
+YELLOW='\033[1;33m'
+
 echo ""
-echo "Checking all the files with Pyflakes"
+echo -e "${GREEN}" "Checking all the files with Pyflakes ${NC}"
 for f in *.py; do echo; echo "Checking $f"; pyflakes "$f"; done
 echo ""
-echo "Done"
+echo -e "${GREEN}" "Checking all the files with Flake8 ${NC}"
+for f in *.py; do echo; echo "Checking $f"; echo "$(flake8 "${f}" --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics) errors"; done
+echo ""
+echo -e "${YELLOW}" "Done! ${NC}"
