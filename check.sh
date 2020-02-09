924e9a083b538636e04d71cd17d1edb44b76b92d
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-02-09 12:52:42 +0530

    Update: check.sh

--- check.sh
+++ check.sh
@@ -7,9 +7,9 @@ YELLOW='\033[1;33m'
 
 echo ""
 echo -e "${GREEN}" "Checking all the files with Pyflakes ${NC}"
-for f in *.py; do echo ""; echo "Checking $f"; pyflakes "$f"; done
+for f in *.py; do echo ""; echo "Checking '$f'"; pyflakes "$f"; done
 echo ""
 echo -e "${GREEN}" "Checking all the files with Flake8 ${NC}"
-for f in *.py; do echo ""; echo "Checking $f"; echo "$(flake8 "${f}" --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics) errors"; done
+for f in *.py; do echo ""; echo "Checking '$f'"; echo "$(flake8 "${f}" --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics) errors"; done
 echo ""
 echo -e "${YELLOW}" "Done! ${NC}"
