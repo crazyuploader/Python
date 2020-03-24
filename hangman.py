adbf4d5ee565a49251bf45af05b47dc8522042c5
akashpythoniee <51360466+akashpythoniee@users.noreply.github.com>
2020-03-24 20:09:12 +0530

    using dictionary instead of list

--- hangman.py
+++ hangman.py
@@ -12,9 +12,9 @@ print("")
 print(" ~ You have 5 attempts to guess it right")
 attempt = 5
 win = 0
-total = [
+total = {
+	"Flower":
     [
-        "Flower",
         "rose",
         "jasmine",
         "lilly",
@@ -24,19 +24,9 @@ total = [
         "cherry",
         "sunflower",
     ],
-    [
-        "Animal",
-        "dog",
-        "cat",
-        "elephant",
-        "horse",
-        "lion",
-        "tiger",
-        "cheetah",
-        "bear",
-    ],
-    [
-        "Computer Part",
+   "Animal": [ "dog", "cat", "elephant", "horse", "lion", "tiger", "cheetah", "bear",],
+    "Computer Part" : [
+        
         "mouse",
         "keyoard",
         "monitor",
@@ -45,8 +35,8 @@ total = [
         "motherboard",
         "pendrive",
     ],
-    [
-        "Body Part",
+    "Body Part" : [
+        
         "fingers",
         "toe",
         "leg",
@@ -58,10 +48,10 @@ total = [
         "hips",
         "mouth",
     ],
-]
-random_list = total[random.randint(0, len(total) - 1)]
-random_word = random.choice(random_list[1::])
-print("Hint: It's", random_list[0])
+}
+random_list = random.choice(list(total.keys()))
+random_word = random.choice(total[random_list])
+print("Hint: It's", random_list)
 print("Picking Random word... Done!")
 while attempt >= 1:
     print("")
@@ -69,7 +59,7 @@ while attempt >= 1:
         print("Hint 1: First letter is --->", random_word[0])
     elif attempt == 3:
         print("Hint 2: Second letter is --->", random_word[1])
-    print("Enter {0} Name:".format(random_list[0]))
+    print("Enter {0} Name:".format(random_list))
     input_word = input().lower().replace(" ", "")
     if input_word == random_word:
         win += 1
