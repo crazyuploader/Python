0862d77c6c67a7fa51c76e116506cf1312aa7ef5
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-03-25 20:25:01 +0530

    Update: ccalc.py

--- ccalc.py
+++ ccalc.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 __author__ = "Jugal Kishore"
-__version__ = "1.0"
+__version__ = "1.1"
 
 import re
 
+previous = 0
 run = True
 print("///Advanced Calculator///\n")
+print("Type 'quit' to exit\n")
 while run:
-    print("Type 'quit' to exit\n")
-    equation = input("Kindly enter an equation\n")
+    if previous == 0:
+        equation = input("Enter equation:\n")
+    else:
+        equation = input(str(previous))
     if equation == "quit":
         print("Uh-Oh, you decided to exit.")
         print("Exiting!")
         break
     else:
         equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
-        equation = eval(equation)
-        print(equation)
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
 print("\nCreated by Jugal Kishore -- 2020")
