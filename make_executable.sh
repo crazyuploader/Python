1dc2646d454050362db48fea5ae629eb3aa936bb
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-03-25 22:32:02 +0530

    Update: make_executable.sh

--- make_executable.sh
+++ make_executable.sh
@@ -7,6 +7,33 @@ NC="\033[0m"
 GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 
+# Variables
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+DATE="$(date +%m/%d/%y)"
+
 git remote -v
-git config user.email
-git config user.name
+git remote remove origin
+git add origin https://"${GH_REF}"
+echo ""
+echo "Adding executable permission"
+echo ""
+chmod +x ./*.py
+CHANGES=$(git status --porcelain)
+if [[ -n ${CHANGES} ]]; then
+    CHANGED_FILES=$(git status --porcelain | cut -d " " -f 3)
+fi
+if [[ -z ${CHANGES} ]]; then
+    echo -e "${GREEN}Nothing to commit${NC}"
+else
+    git config user.email "49350241+crazyuploader@users.noreply.github.com"
+    git config user.name "crazyuploader"
+    git add .
+    git commit -m "Travis CI" \
+               -m "" \
+               -m "Date: ${DATE}" \
+               -m "" \
+               -m "Add Executable Permission:" \
+               -m "$(for changes in ${CHANGED_FILES}; do echo "${changes}"; done)"
+    git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GH_REF}"
+    echo -e "${YELLOW}Changes pushed to branch '${BRANCH}' at https://${GH_REF}" HEAD:"${BRANCH}"
+fi
