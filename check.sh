654e287dc0665250cb05b3672d0086308b48af9b
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-17 20:04:13 +0530

    Update: check.sh

--- check.sh
+++ check.sh
@@ -4,20 +4,36 @@
 GREEN="\033[1;32m"
 NC="\033[0m"
 YELLOW="\033[1;33m"
+RED="\033[0;31m"
 
-clear
-echo -e "${YELLOW}Python Linter${NC}"
 echo ""
-echo -e "${GREEN}Checking all the files with Pyflakes ${NC}"
-for f in *.py; do echo ""; echo "Checking '${f}'"; pyflakes "${f}"; done
-echo ""
-echo -e "${YELLOW}Pyflakes Done!${NC}"
-echo ""
-echo -e "${GREEN}Checking all the files with Flake8 ${NC}"
-for f in *.py; do echo ""; echo "Checking '${f}'"; echo "$(flake8 "${f}" --count --ignore=C901 --exit-zero --max-complexity=10 --max-line-length=127 --statistics) errors"; done
-echo ""
-echo -e "${YELLOW}Flake8 Done!${NC}"
+echo -e "${GREEN}" "Available files -${NC}"
+LIST_FILES="$(find . -path ./.git -prune -o -name '*.py' -print | sed 's|^./||')"
 FILES=0
-for f in *.py; do ((FILES = FILES + 1)); done
+for file in ${LIST_FILES}; do
+    echo "$file"
+    ((FILES = FILES + 1))
+done
+echo ""
+echo -e "${GREEN}Python Linter ${NC}"
+echo ""
+echo -e "${YELLOW}Checking all the files with Pyflakes${NC}"
+echo ""
+for file in ${LIST_FILES}; do
+    echo "Checking '${file}'"
+    pyflakes "${file}">> /dev/null 2>&1
+    ERROR_CODE="$?"
+    if [[ ${ERROR_CODE} != "0" ]]; then
+        echo ""
+        echo -e "${RED}$(pyflakes "${file}")${NC}"
+    fi
+done
+echo ""
+echo -e "${YELLOW}Checking all the files with Flake8 ${NC}"
+echo ""
+for file in ${LIST_FILES}; do
+    echo "Checking '${file}'"
+    echo "$(flake8 "${file}" --count --ignore=C901 --exit-zero --max-complexity=10 --max-line-length=127 --statistics) errors"
+done
 echo ""
 echo -e "Number of Python Files Checked: ${GREEN}${FILES}${NC}"
