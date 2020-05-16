a6c7d7c34eeabe91925f9290f2f4c63c4aa08eb4
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-05-16 19:45:29 +0530

    Update: corona_updates_india.py

--- corona_updates_india.py
+++ corona_updates_india.py
@@ -17,7 +17,7 @@ cases = []
 recovered = []
 deaths = []
 
-for i in range(1, 33):
+for i in range(1, 34):
     state.append((soup.find("table").find_all("tr")[i].find_all("td")[1].contents[0]))
     cases.append((soup.find("table").find_all("tr")[i].find_all("td")[2].contents[0]))
     recovered.append(
@@ -27,7 +27,7 @@ for i in range(1, 33):
 
 output = "State, Cases, Recovered, Deaths\n"
 file = open("data.csv", "w")
-for i in range(0, 32):
+for i in range(0, 33):
     output = (
         output
         + "\n"
@@ -36,6 +36,7 @@ for i in range(0, 32):
 file.write(output)
 file.close()
 os.system("csvtomd data.csv")
+os.system("rm data.csv")
 print("")
 print("Website Scrapped:", URL)
 print("\nCreated by Jugal Kishore & Akash Shiva -- 2020")
