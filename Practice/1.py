897c5a76337e39638eccc2810b65994b17588ac0
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-06-11 21:14:51 +0530

    Create Practice/1.py

---
+++ Practice/1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+__author__ = "Jugal Kishore"
+__version__ = "1.0"
+
+
+# Display numbers divisible by 7 but not 5 in the range 2000, 3200
+print("Number(s) divisible by 7 but not 5 in the range 2000, 3200 -\n")
+number = []
+for i in range(2000, 3200):
+    if (i%7==0) and (i%5!=0):
+        number.append(str(i))
+print(",".join(number))
