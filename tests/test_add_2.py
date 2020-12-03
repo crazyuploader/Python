ff9952eb9b01d3afa79b434dc83045e853029e17
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-12-03 17:50:55 +0530

    Add Python Test

---
+++ tests/test_add_2.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+# Simple test to check addition of two numbers
+
+def Add_2(x, y):
+    return x + y
+
+
+def test_method_1():
+    """
+    Call Add_2() for integors
+    """
+    assert Add_2(5, 8) == 13
+
+
+def test_method_2():
+    """
+    Call Add_2() for floating point
+    """
+    assert Add_2(5.2, 6.8) == 12
+
