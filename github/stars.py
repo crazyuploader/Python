392f4f180840ed3eb187a4eb7933394c1fdad4d1
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-01-14 14:21:07 +0530

    Create stars.py

---
+++ github/stars.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Displays GitHub Starred Repo(s) for a given user"""
+
+import requests
+import os
+
+__author__ = "Jugal Kishore"
+
+
+def clear():
+    if os.name == "posix":
+        os.system("clear")
+
+
+USER = str(input("Enter GitHub Username: "))
+if not USER:
+    print("Enter something!")
+    exit(1)
+clear()
+URL = f"https://api.github.com/users/{USER}/starred"
+got = requests.get(URL)
+JSON = got.json()
+print(f"Fetching Details for {USER}...")
+if got.status_code == 200:
+    clear()
+    total_requests = got.headers["X-RateLimit-Limit"]
+    remaining_requests = got.headers["X-RateLimit-Remaining"]
+    print(f"GitHub API Used: {remaining_requests}/{total_requests}")
+    print("")
+    if len(JSON) == 0:
+        print(f"GitHub User '{USER}' doesn't have any Starred Repo(s)")
+        exit(0)
+    print(f"Details for GitHub User '{USER}'")
+    print(f"Last {len(JSON)} Starred Repo(s)")
+    for repo in JSON:
+        print("")
+        print(f"         Name: {repo['name']}")
+        print(f"    Full Name: {repo['full_name']}")
+        print(f"  Description: {repo['description']}")
+        print(f"     Language: {repo['language']}")
+        print(f"        Stars: {repo['stargazers_count']}")
+        print(f"    Clone URL: {repo['clone_url']}")
+else:
+    print("ERROR: Check username")
