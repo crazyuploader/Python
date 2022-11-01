5d8f5d736c48b687061f4a7c0c62e2dd7a05590d
Jugal Kishore <me@devjugal.com>
2022-11-01 22:48:17 +0530

    Create get_latest_speedtest.py

---
+++ other/get_latest_speedtest.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# filename: get_latest_speedtest.py
+"""Get latest binaries for Ookla Speedtest CLI"""
+
+__author__ = "Jugal Kishore <me@devjugal.com>"
+
+
+# Import Libraries
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+# Get HTML from speedtest.net website
+print("Getting HTML from speedtest.net website...")
+data = requests.get("https://www.speedtest.net/apps/cli")
+print(f"HTTP Status Code: {data.status_code}")
+if data.status_code != 200:
+    print("Fatal: Error getting HTML file from: https://www.speedtest.net/apps/cli")
+    print("Exiting!")
+    exit(1)
+else:
+    print("200 HTTP Status Code, continuing...")
+
+# Souping
+soup = BeautifulSoup(data.text, "html.parser")
+links = soup.find(id="linux-flyout")
+dicts = {}
+for link in links:
+    tag = link.find("a")
+    dicts[tag.string] = tag.get("href")
+
+print(json.dumps(dicts, indent = 4))
